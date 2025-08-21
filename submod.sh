01d8aaf12956c17abcf856aadb517cd59d194711
Gergo Koteles <soyer@irl.hu>
2025-08-21 12:50:51 +0200

    Update to circle-stdlib v17 (circle50)

--- submod.sh
+++ submod.sh
@@ -5,14 +5,14 @@ set -ex
 git submodule update --init --recursive -f
 
 # Use fixed master branch of circle-stdlib then re-update
-cd circle-stdlib/
-git checkout -f --recurse-submodules 1111eee # Matches Circle Step49
-cd -
+#cd circle-stdlib/
+#git checkout -f --recurse-submodules 1111eee # Matches Circle Step49
+#cd -
 
 # Optional update submodules explicitly
-cd circle-stdlib/libs/circle
-git checkout -f --recurse-submodules f18c60fa38042ea7132533e658abfafd5bd63435
-cd -
+#cd circle-stdlib/libs/circle
+#git checkout -f --recurse-submodules f18c60fa38042ea7132533e658abfafd5bd63435
+#cd -
 #cd circle-stdlib/libs/circle-newlib
 #git checkout develop
 #cd -
