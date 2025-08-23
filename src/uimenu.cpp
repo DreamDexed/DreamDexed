0ae8c0cc71763c93299a703e89fe473f23c6d1ca
Gergo Koteles <soyer@irl.hu>
2025-08-23 08:54:18 +0200

    CUIMenu: add note limits and shift

--- src/uimenu.cpp
+++ src/uimenu.cpp
@@ -84,6 +84,7 @@ const CUIMenu::TMenuItem CUIMenu::s_TGMenu[] =
 	{"Resonance",	EditTGParameter,	0,	CMiniDexed::TGParameterResonance},
 	{"Pitch Bend",	MenuHandler,		s_EditPitchBendMenu},
 	{"Portamento",		MenuHandler,		s_EditPortamentoMenu},
+	{"Note Limit",		MenuHandler,	s_EditNoteLimitMenu},
 	{"Poly/Mono",		EditTGParameter,	0,	CMiniDexed::TGParameterMonoMode}, 
 	{"Modulation",		MenuHandler,		s_ModulationMenu},
 	{"Channel",	EditTGParameter,	0,	CMiniDexed::TGParameterMIDIChannel},
@@ -126,6 +127,14 @@ const CUIMenu::TMenuItem CUIMenu::s_EditPortamentoMenu[] =
 	{"Mode",		EditTGParameter2,	0,	CMiniDexed::TGParameterPortamentoMode},
 	{"Glissando",		EditTGParameter2,	0,	CMiniDexed::TGParameterPortamentoGlissando},
 	{"Time",		EditTGParameter2,	0,	CMiniDexed::TGParameterPortamentoTime},
+	{0}
+};
+
+const CUIMenu::TMenuItem CUIMenu::s_EditNoteLimitMenu[] =
+{
+	{"Limit Low",		EditTGParameter2,	0,	CMiniDexed::TGParameterNoteLimitLow},
+	{"Limit High",		EditTGParameter2,	0,	CMiniDexed::TGParameterNoteLimitHigh},
+	{"Shift",		EditTGParameter2,	0,	CMiniDexed::TGParameterNoteShift},
 	{0}
 };
 
@@ -311,6 +320,9 @@ CUIMenu::TParameter CUIMenu::s_TGParameter[CMiniDexed::TGParameterUnknown] =
 	{0,	1,					1, ToPortaMode},	// TGParameterPortamentoMode
 	{0,	1,					1, ToPortaGlissando},	// TGParameterPortamentoGlissando
 	{0,	99,					1},			// TGParameterPortamentoTime
+	{0,	127,					1, ToMIDINote},		// TGParameterNoteLimitLow
+	{0,	127,					1, ToMIDINote},		// TGParameterNoteLimitHigh
+	{-24,	24,					1, ToMIDINoteShift},	// TGParameterNoteShift
 	{0,	1,					1, ToPolyMono}, 		// TGParameterMonoMode 
 	{0, 99, 1}, //MW Range
 	{0, 1, 1, ToOnOff}, //MW Pitch
@@ -405,6 +417,30 @@ const char CUIMenu::s_NoteName[100][5] =
 "A5", "A#5", "B5", "C6", "C#6", "D6", "D#6", "E6", "F6", "F#6", "G6", "G#6",
 "A6", "A#6", "B6", "C7", "C#7", "D7", "D#7", "E7", "F7", "F#7", "G7", "G#7",
 "A7", "A#7", "B7", "C8"
+};
+
+const char CUIMenu::s_MIDINoteName[128][9] =
+{
+"0   C-2", "1   C#-2", "2   D-2", "3   D#-2", "4   E-2", "5   F-2", "6   F#-2", "7   G-2", "8   G#-2", "9   A-2", "10  A#-2", "11  B-2",
+"12  C-1", "13  C#-1", "14  D-1", "15  D#-1", "16  E-1", "17  F-1", "18  F#-1", "19  G-1", "20  G#-1", "21  A-1", "22  A#-1", "23  B-1",
+"24  C0",  "25  C#0",  "26  D0",  "27  D#0",  "28  E0",  "29  F0",  "30  F#0",  "31  G0",  "32  G#0",  "33  A0",  "34  A#0",  "35  B0",
+"36  C1",  "37  C#1",  "38  D1",  "39  D#1",  "40  E1",  "41  F1",  "42  F#1",  "43  G1",  "44  G#1",  "45  A1",  "46  A#1",  "47  B1",
+"48  C2",  "49  C#2",  "50  D2",  "51  D#2",  "52  E2",  "53  F2",  "54  F#2",  "55  G2",  "56  G#2",  "57  A2",  "58  A#2",  "59  B2",
+"60  C3",  "61  C#3",  "62  D3",  "63  D#3",  "64  E3",  "65  F3",  "66  F#3",  "67  G3",  "68  G#3",  "69  A3",  "70  A#3",  "71  B3",
+"72  C4",  "73  C#4",  "74  D4",  "75  D#4",  "76  E4",  "77  F4",  "78  F#4",  "79  G4",  "80  G#4",  "81  A4",  "82  A#4",  "83  B4",
+"84  C5",  "85  C#5",  "86  D5",  "87  D#5",  "88  E5",  "89  F5",  "90  F#5",  "91  G5",  "92  G#5",  "93  A5",  "94  A#5",  "95  B5",
+"96  C6",  "97  C#6",  "98  D6",  "99  D#6",  "100 E6",  "101 F6",  "102 F#6",  "103 G6",  "104 G#6",  "105 A6",  "106 A#6",  "107 B6",
+"108 C7",  "109 C#7",  "110 D7",  "111 D#7",  "112 E7",  "113 F7",  "114 F#7",  "115 G7",  "116 G#7",  "117 A7",  "118 A#7",  "119 B7",
+"120 C8",  "121 C#8",  "122 D8",  "123 D#8",  "124 E8",  "125 F8",  "126 F#8",  "127 G8"
+};
+
+const char CUIMenu::s_MIDINoteShift[49][8] =
+{
+"-24 C1", "-23 C#1", "-22 D1", "-21 D#1", "-20 E1", "-19 F1", "-18 F#1", "-17 G1", "-16 G#1", "-15 A1", "-14 A#1", "-13 B1",
+"-12 C2", "-11 C#2", "-10 D2", "-9  D#2", "-8  E2", "-7  F2", "-6  F#2", "-5  G2", "-4  G#2", "-3  A2", "-2  A#2", "-1  B2",
+"0   C3", "+1  C#3", "+2  D3", "+3  D#3", "+4  E3", "+5  F3", "+6  F#3", "+7  G3", "+8  G#3", "+9  A3", "+10 A#3", "+11 B3",
+"+12 C4", "+13 C#4", "+14 D4", "+15 D#4", "+16 E4", "+17 F4", "+18 F#4", "+19 G4", "+20 G#4", "+21 A4", "+22 A#4", "+23 B4",
+"+24 C5"
 };
 
 static const unsigned NoteC3 = 39;
@@ -1255,6 +1291,20 @@ std::string CUIMenu::ToBreakpointNote (int nValue, int nWidth)
 	assert ((unsigned) nValue < sizeof s_NoteName / sizeof s_NoteName[0]);
 
 	return s_NoteName[nValue];
+}
+
+std::string CUIMenu::ToMIDINote (int nValue, int nWidth)
+{
+	assert ((unsigned) nValue < sizeof s_MIDINoteName / sizeof *s_MIDINoteName);
+
+	return s_MIDINoteName[nValue];
+}
+
+std::string CUIMenu::ToMIDINoteShift (int nValue, int nWidth)
+{
+	assert ((unsigned) nValue + 24 < sizeof s_MIDINoteShift / sizeof *s_MIDINoteShift);
+
+	return s_MIDINoteShift[nValue + 24];
 }
 
 std::string CUIMenu::ToKeyboardCurve (int nValue, int nWidth)
