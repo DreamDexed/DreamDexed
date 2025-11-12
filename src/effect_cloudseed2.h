50c72df1b1e384204133eeb2275c10f71c51190c
Gergo Koteles <soyer@irl.hu>
2025-11-12 21:11:47 +0100

    effect_cloudseed2: convert to tabs

--- src/effect_cloudseed2.h
+++ src/effect_cloudseed2.h
@@ -58,139 +58,139 @@ public:
 	static unsigned getIDFromPresetName(const char *presetName)
 	{
 		for (unsigned i = 0; i < presets_num; ++i)
-		    if (strcmp(PresetNames[i], presetName) == 0)
-			    return i;
+			if (strcmp(PresetNames[i], presetName) == 0)
+				return i;
 
 		return 0;
 	}
 
-    AudioEffectCloudSeed2(float samplerate):
-    samplerate{samplerate},
-    ramp_dt{10.0f / samplerate},
-    engine{(int)samplerate},
-    targetVol{},
-    needBufferClear{},
-    waitBufferClear{},
-    needParameterLoad{},
-    preset{},
-    vol{}
-    {
-    }
+	AudioEffectCloudSeed2(float samplerate):
+	samplerate{samplerate},
+	ramp_dt{10.0f / samplerate},
+	engine{(int)samplerate},
+	targetVol{},
+	needBufferClear{},
+	waitBufferClear{},
+	needParameterLoad{},
+	preset{},
+	vol{}
+	{
+	}
 
-    void setParameter(unsigned paramID, float param)
-    {
-        engine.SetParameter(paramID, param);
-    }
+	void setParameter(unsigned paramID, float param)
+	{
+		engine.SetParameter(paramID, param);
+	}
 
-    float getParameter(unsigned paramID)
-    {
-        return engine.GetAllParameters()[paramID];
-    }
+	float getParameter(unsigned paramID)
+	{
+		return engine.GetAllParameters()[paramID];
+	}
 
-    void process(float32_t* inblockL, float32_t* inblockR, uint16_t len)
-    {
-        if (targetVol == 0.0f && vol > 0.0f)
-        {
-            engine.Process(inblockL, inblockR, inblockL, inblockR, len);
-            for (uint16_t i = 0; i < len; ++i)
-            {
-                vol = std::max(0.0f, vol - ramp_dt);
-                inblockL[i] *= vol;
-                inblockR[i] *= vol;
-            }
+	void process(float32_t* inblockL, float32_t* inblockR, uint16_t len)
+	{
+		if (targetVol == 0.0f && vol > 0.0f)
+		{
+			engine.Process(inblockL, inblockR, inblockL, inblockR, len);
+			for (uint16_t i = 0; i < len; ++i)
+			{
+				vol = std::max(0.0f, vol - ramp_dt);
+				inblockL[i] *= vol;
+				inblockR[i] *= vol;
+			}
 
-            return;
-        }
+			return;
+		}
 
-        if (needBufferClear)
-        {
-            engine.StartSlowClear();
-            needBufferClear = false;
-            waitBufferClear = true;
-        }
+		if (needBufferClear)
+		{
+			engine.StartSlowClear();
+			needBufferClear = false;
+			waitBufferClear = true;
+		}
 
-        if (waitBufferClear)
-        {
-            if (engine.SlowClearDone(SLOW_CLEAR_SIZE))
-                waitBufferClear = false;
+		if (waitBufferClear)
+		{
+			if (engine.SlowClearDone(SLOW_CLEAR_SIZE))
+				waitBufferClear = false;
 
-            memset(inblockL, 0, len * sizeof *inblockL);
-            memset(inblockR, 0, len * sizeof *inblockR);
+			memset(inblockL, 0, len * sizeof *inblockL);
+			memset(inblockR, 0, len * sizeof *inblockR);
 
-            return;
-        }
+			return;
+		}
 
-        if (needParameterLoad)
-        {
-            unsigned needParam = needParameterLoad;
-            unsigned paramID = Cloudseed::Parameter::COUNT - needParam;
-            engine.SetParameter(paramID, Presets[preset][paramID]);
-            needParameterLoad = needParam - 1;
-            
-            memset(inblockL, 0, len * sizeof *inblockL);
-            memset(inblockR, 0, len * sizeof *inblockR);
+		if (needParameterLoad)
+		{
+			unsigned needParam = needParameterLoad;
+			unsigned paramID = Cloudseed::Parameter::COUNT - needParam;
+			engine.SetParameter(paramID, Presets[preset][paramID]);
+			needParameterLoad = needParam - 1;
+			
+			memset(inblockL, 0, len * sizeof *inblockL);
+			memset(inblockR, 0, len * sizeof *inblockR);
 
-            if (!needParameterLoad)
-                targetVol = 1.0f;
+			if (!needParameterLoad)
+				targetVol = 1.0f;
 
-            return;
-        }
+			return;
+		}
 
-        if (targetVol == 1.0f && vol < 1.0f)
-        {
-            engine.Process(inblockL, inblockR, inblockL, inblockR, len);
-            for (uint16_t i = 0; i < len; ++i)
-            {
-                vol = std::min(vol + ramp_dt, 1.0f);
-                inblockL[i] *= vol;
-                inblockR[i] *= vol;
-            }
+		if (targetVol == 1.0f && vol < 1.0f)
+		{
+			engine.Process(inblockL, inblockR, inblockL, inblockR, len);
+			for (uint16_t i = 0; i < len; ++i)
+			{
+				vol = std::min(vol + ramp_dt, 1.0f);
+				inblockL[i] *= vol;
+				inblockR[i] *= vol;
+			}
 
-            return;
-        } 
+			return;
+		} 
 
-        if (isDisabled()) return;
+		if (isDisabled()) return;
 
-        engine.Process(inblockL, inblockR, inblockL, inblockR, len);
-    }
+		engine.Process(inblockL, inblockR, inblockL, inblockR, len);
+	}
 
-    void loadPreset(unsigned p)
-    {
-        assert(p < presets_num);
-        preset = p;
+	void loadPreset(unsigned p)
+	{
+		assert(p < presets_num);
+		preset = p;
 
-        targetVol = 0.0f;
-        needBufferClear = true;
-        needParameterLoad = Cloudseed::Parameter::COUNT;
-    }
+		targetVol = 0.0f;
+		needBufferClear = true;
+		needParameterLoad = Cloudseed::Parameter::COUNT;
+	}
 
-    void setNeedBufferClear()
-    {
-        needBufferClear = true;
-    }
+	void setNeedBufferClear()
+	{
+		needBufferClear = true;
+	}
 
-    void setRampedDown()
-    {
-        vol = 0.0f;
-    }
+	void setRampedDown()
+	{
+		vol = 0.0f;
+	}
 
-    bool isDisabled()
-    {
-        double *params = engine.GetAllParameters();
-        return params[Cloudseed::Parameter::DryOut] == 1.0f &&
-                params[Cloudseed::Parameter::EarlyOut] == 0.0f &&
-                params[Cloudseed::Parameter::LateOut] == 0.0f;
-    }
+	bool isDisabled()
+	{
+		double *params = engine.GetAllParameters();
+		return params[Cloudseed::Parameter::DryOut] == 1.0f &&
+			params[Cloudseed::Parameter::EarlyOut] == 0.0f &&
+			params[Cloudseed::Parameter::LateOut] == 0.0f;
+	}
 
 private:
-    float samplerate;
-    float ramp_dt;
-    Cloudseed::ReverbController engine;
-    std::atomic<float> targetVol;
-    std::atomic<bool> needBufferClear;
-    bool waitBufferClear;
-    std::atomic<unsigned> needParameterLoad;
-    std::atomic<unsigned> preset;
+	float samplerate;
+	float ramp_dt;
+	Cloudseed::ReverbController engine;
+	std::atomic<float> targetVol;
+	std::atomic<bool> needBufferClear;
+	bool waitBufferClear;
+	std::atomic<unsigned> needParameterLoad;
+	std::atomic<unsigned> preset;
 
-    std::atomic<float> vol;
+	std::atomic<float> vol;
 };
