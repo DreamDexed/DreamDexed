61490f9586d59afc293aa4046d7518649069c58f
Gergo Koteles <soyer@irl.hu>
2025-12-17 14:26:16 +0100

    zyn: Chorus: convert indentation to tabs

--- src/zyn/Chorus.h
+++ src/zyn/Chorus.h
@@ -32,99 +32,99 @@ namespace zyn {
 // DUAL   180° dual phase chorus
 
 #define CHORUS_MODES \
-    CHORUS,\
-    FLANGE,\
-    TRIPLE,\
-    DUAL
+	CHORUS,\
+	FLANGE,\
+	TRIPLE,\
+	DUAL
 
 /**Chorus and Flange effects*/
 class Chorus final:public Effect
 {
-    public:
-        enum ChorusModes {
-            CHORUS_MODES
-        };
+public:
+	enum ChorusModes {
+		CHORUS_MODES
+	};
 
-        Chorus(EffectParams pars);
-        /**Destructor*/
-        ~Chorus();
-        void out(const Stereo<float *> &input);
-        unsigned char getpresetpar(unsigned char npreset, unsigned int npar);
-        void setpreset(unsigned char npreset);
-        /**
-         * Sets the value of the chosen variable
-         *
-         * The possible parameters are:
-         *   -# Volume
-         *   -# Panning
-         *   -# LFO Frequency
-         *   -# LFO Randomness
-         *   -# LFO Type
-         *   -# LFO stereo
-         *   -# Depth
-         *   -# Delay
-         *   -# Feedback
-         *   -# Flange Mode
-         *   -# Subtractive
-         * @param npar number of chosen parameter
-         * @param value the new value
-         */
-        void changepar(int npar, unsigned char value);
-        /**
-         * Gets the value of the chosen variable
-         *
-         * The possible parameters are:
-         *   -# Volume
-         *   -# Panning
-         *   -# LFO Frequency
-         *   -# LFO Randomness
-         *   -# LFO Type
-         *   -# LFO stereo
-         *   -# Depth
-         *   -# Delay
-         *   -# Feedback
-         *   -# Flange Mode
-         *   -# Subtractive
-         * @param npar number of chosen parameter
-         * @return the value of the parameter
-         */
-        unsigned char getpar(int npar) const;
-        void cleanup(void);
+	Chorus(EffectParams pars);
+	/**Destructor*/
+	~Chorus();
+	void out(const Stereo<float *> &input);
+	unsigned char getpresetpar(unsigned char npreset, unsigned int npar);
+	void setpreset(unsigned char npreset);
+	/**
+	 * Sets the value of the chosen variable
+	 *
+	 * The possible parameters are:
+	 * -# Volume
+	 * -# Panning
+	 * -# LFO Frequency
+	 * -# LFO Randomness
+	 * -# LFO Type
+	 * -# LFO stereo
+	 * -# Depth
+	 * -# Delay
+	 * -# Feedback
+	 * -# Flange Mode
+	 * -# Subtractive
+	 * @param npar number of chosen parameter
+	 * @param value the new value
+	 */
+	void changepar(int npar, unsigned char value);
+	/**
+	 * Gets the value of the chosen variable
+	 *
+	 * The possible parameters are:
+	 * -# Volume
+	 * -# Panning
+	 * -# LFO Frequency
+	 * -# LFO Randomness
+	 * -# LFO Type
+	 * -# LFO stereo
+	 * -# Depth
+	 * -# Delay
+	 * -# Feedback
+	 * -# Flange Mode
+	 * -# Subtractive
+	 * @param npar number of chosen parameter
+	 * @return the value of the parameter
+	 */
+	unsigned char getpar(int npar) const;
+	void cleanup(void);
 
-        static rtosc::Ports ports;
-    private:
-        inline float getSample(float* delayline, float mdel, int dk);
+	static rtosc::Ports ports;
+private:
+	inline float getSample(float* delayline, float mdel, int dk);
 
-        //Chorus Parameters
-        unsigned char Pvolume;
-        unsigned char Pdepth;      //the depth of the Chorus(ms)
-        unsigned char Pdelay;      //the delay (ms)
-        unsigned char Pfb;         //feedback
-        unsigned char Pflangemode; //mode as described above in CHORUS_MODES
-        unsigned char Poutsub;     //if I wish to subtract the output instead of the adding it
-        EffectLFO     lfo;         //lfo-ul chorus
+	//Chorus Parameters
+	unsigned char Pvolume;
+	unsigned char Pdepth;		//the depth of the Chorus(ms)
+	unsigned char Pdelay;		//the delay (ms)
+	unsigned char Pfb;		//feedback
+	unsigned char Pflangemode;	//mode as described above in CHORUS_MODES
+	unsigned char Poutsub;		//if I wish to subtract the output instead of the adding it
+	EffectLFO lfo;			//lfo-ul chorus
 
 
-        //Parameter Controls
-        void setvolume(unsigned char _Pvolume);
-        void setdepth(unsigned char _Pdepth);
-        void setdelay(unsigned char _Pdelay);
-        void setfb(unsigned char _Pfb);
+	//Parameter Controls
+	void setvolume(unsigned char _Pvolume);
+	void setdepth(unsigned char _Pdepth);
+	void setdelay(unsigned char _Pdelay);
+	void setfb(unsigned char _Pfb);
 
-        //Internal Values
-        float depth, delay, fb;
-        float dlHist, dlNew, lfol;
-        float drHist, drNew, lfor;
-        float dlHist2, dlNew2;
-        float drHist2, drNew2;
-        float dlHist3, dlNew3;
-        float drHist3, drNew3;
-        int   maxdelay;
-        Stereo<float *> delaySample;
-        int dlk, drk;
-        float getdelay(float xlfo);
+	//Internal Values
+	float depth, delay, fb;
+	float dlHist, dlNew, lfol;
+	float drHist, drNew, lfor;
+	float dlHist2, dlNew2;
+	float drHist2, drNew2;
+	float dlHist3, dlNew3;
+	float drHist3, drNew3;
+	int maxdelay;
+	Stereo<float *> delaySample;
+	int dlk, drk;
+	float getdelay(float xlfo);
 
-        float output;
+	float output;
 };
 
 }
