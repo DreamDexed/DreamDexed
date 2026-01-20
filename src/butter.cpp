01312dedaaf3032f8e7baacfda2cb169dff9800b
Gergo Koteles <soyer@irl.hu>
2026-01-20 23:23:58 +0100

    butter: cleanup

--- src/butter.cpp
+++ src/butter.cpp
@@ -36,11 +36,13 @@
  *
  */
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+#include <cassert>
 #include <math.h>
 #include "butter.h"
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846264338327950288
+#endif
 
 /**********************************************************************
   binomial_mult - multiplies a series of binomials together and returns
@@ -59,9 +61,7 @@
   
   The a[i] coefficients can in general be complex but should in most
   cases turn out to be real. The a[i] coefficients are returned by the
-  function as a pointer to an array of doubles of length 2n. Storage
-  for the array is allocated by the function and should be freed by the
-  calling program when no longer needed.
+  function as a pointer to an array of doubles of length 2n.
   
   Function arguments:
   
@@ -70,15 +70,12 @@
         assumed to be the real part of the coefficient of the ith binomial
         and p[2i+1] is assumed to be the imaginary part. The overall size
         of the array is then 2n.
+  a  -  Pointer to the coefficients array of doubles. The overall size is 2n.
 */
 
-double *binomial_mult( int n, double *p )
+static void binomial_mult( int n, double *p, double *a )
 {
     int i, j;
-    double *a;
-
-    a = (double *)calloc( 2 * n, sizeof(double) );
-    if( a == NULL ) return( NULL );
 
     for( i = 0; i < n; ++i )
     {
@@ -90,7 +87,6 @@ double *binomial_mult( int n, double *p )
 	a[0] += p[2*i];
 	a[1] += p[2*i+1];
     }
-    return( a );
 }
 
 
@@ -112,26 +108,20 @@ double *binomial_mult( int n, double *p )
   x^2n + a[0]*x^2n-1 + a[1]*x^2n-2 + ... +a[2n-2]*x + a[2n-1]
   
   The a[i] coefficients can in general be complex but should in most cases
-  turn out to be real. The a[i] coefficients are returned by the function as
-  a pointer to an array of doubles of length 4n. The real and imaginary
-  parts are stored, respectively, in the even and odd elements of the array.
-  Storage for the array is allocated by the function and should be freed by
-  the calling program when no longer needed.
+  turn out to be real. The real and imaginary parts are stored, respectively,
+  in the even and odd elements of the array.
   
   Function arguments:
   
   n  -  The number of trinomials to multiply
   b  -  Pointer to an array of doubles of length 2n.
   c  -  Pointer to an array of doubles of length 2n.
+  a  -  Pointer to the coefficents array of doubles of length 4n.
 */
 
-double *trinomial_mult( int n, double *b, double *c )
+static void trinomial_mult( int n, double *b, double *c, double *a )
 {
     int i, j;
-    double *a;
-
-    a = (double *)calloc( 4 * n, sizeof(double) );
-    if( a == NULL ) return( NULL );
 
     a[2] = c[0];
     a[3] = c[1];
@@ -156,18 +146,16 @@ double *trinomial_mult( int n, double *b, double *c )
 	a[0] += b[2*i];
 	a[1] += b[2*i+1];
     }
-
-    return( a );
 }
 
 
 /**********************************************************************
   dcof_bwlp - calculates the d coefficients for a butterworth lowpass 
-  filter. The coefficients are returned as an array of doubles.
-
+  filter.
+  dcof  - Pointer to the coefficients array of doubles. The overall size is 2n.
 */
 
-double *dcof_bwlp( int n, double fcf )
+static void dcof_bwlp( int n, double fcf, double *dcof )
 {
     int k;            // loop variables
     double theta;     // M_PI * fcf / 2.0
@@ -177,11 +165,7 @@ double *dcof_bwlp( int n, double fcf )
     double sparg;     // sine of the pole angle
     double cparg;     // cosine of the pole angle
     double a;         // workspace variable
-    double *rcof;     // binomial coefficients
-    double *dcof;     // dk coefficients
-
-    rcof = (double *)calloc( 2 * n, sizeof(double) );
-    if( rcof == NULL ) return( NULL );
+    double rcof[2 * n] = {}; // binomial coefficients
 
     theta = M_PI * fcf;
     st = sin(theta);
@@ -197,35 +181,34 @@ double *dcof_bwlp( int n, double fcf )
 	rcof[2*k+1] = -st*cparg/a;
     }
 
-    dcof = binomial_mult( n, rcof );
-    free( rcof );
+    binomial_mult( n, rcof, dcof );
 
     dcof[1] = dcof[0];
     dcof[0] = 1.0;
     for( k = 3; k <= n; ++k )
         dcof[k] = dcof[2*k-2];
-    return( dcof );
 }
 
 /**********************************************************************
   dcof_bwhp - calculates the d coefficients for a butterworth highpass 
-  filter. The coefficients are returned as an array of doubles.
-
+  filter.
+  dcof  - Pointer to the coefficients array of doubles. The overall size is 2n.
 */
 
-double *dcof_bwhp( int n, double fcf )
+static void dcof_bwhp( int n, double fcf, double *dcof )
 {
-    return( dcof_bwlp( n, fcf ) );
+    dcof_bwlp( n, fcf, dcof );
 }
 
 
 /**********************************************************************
   dcof_bwbp - calculates the d coefficients for a butterworth bandpass 
   filter. The coefficients are returned as an array of doubles.
+  dcof  - Pointer to the coefficients array of doubles. The overall size is 4n.
 
 */
 
-double *dcof_bwbp( int n, double f1f, double f2f )
+static void dcof_bwbp( int n, double f1f, double f2f, double *dcof )
 {
     int k;            // loop variables
     double theta;     // M_PI * (f2f - f1f) / 2.0
@@ -234,9 +217,8 @@ double *dcof_bwbp( int n, double f1f, double f2f )
     double ct;        // cosine of theta
     double s2t;       // sine of 2*theta
     double c2t;       // cosine 0f 2*theta
-    double *rcof;     // z^-2 coefficients
-    double *tcof;     // z^-1 coefficients
-    double *dcof;     // dk coefficients
+    double rcof[2 * n] = {};     // z^-2 coefficients
+    double tcof[2 * n] = {};     // z^-1 coefficients
     double parg;      // pole angle
     double sparg;     // sine of pole angle
     double cparg;     // cosine of pole angle
@@ -248,9 +230,6 @@ double *dcof_bwbp( int n, double f1f, double f2f )
     ct = cos(theta);
     s2t = 2.0*st*ct;        // sine of 2*theta
     c2t = 2.0*ct*ct - 1.0;  // cosine of 2*theta
-
-    rcof = (double *)calloc( 2 * n, sizeof(double) );
-    tcof = (double *)calloc( 2 * n, sizeof(double) );
 
     for( k = 0; k < n; ++k )
     {
@@ -264,24 +243,21 @@ double *dcof_bwbp( int n, double f1f, double f2f )
 	tcof[2*k+1] = -2.0*cp*st*cparg/a;
     }
 
-    dcof = trinomial_mult( n, tcof, rcof );
-    free( tcof );
-    free( rcof );
+    trinomial_mult( n, tcof, rcof, dcof );
 
     dcof[1] = dcof[0];
     dcof[0] = 1.0;
     for( k = 3; k <= 2*n; ++k )
         dcof[k] = dcof[2*k-2];
-    return( dcof );
 }
 
 /**********************************************************************
   dcof_bwbs - calculates the d coefficients for a butterworth bandstop 
   filter. The coefficients are returned as an array of doubles.
-
+  dcof  - Pointer to the coefficients array of doubles. The overall size is 4n.
 */
 
-double *dcof_bwbs( int n, double f1f, double f2f )
+static void dcof_bwbs( int n, double f1f, double f2f , double *dcof )
 {
     int k;            // loop variables
     double theta;     // M_PI * (f2f - f1f) / 2.0
@@ -290,9 +266,8 @@ double *dcof_bwbs( int n, double f1f, double f2f )
     double ct;        // cosine of theta
     double s2t;       // sine of 2*theta
     double c2t;       // cosine 0f 2*theta
-    double *rcof;     // z^-2 coefficients
-    double *tcof;     // z^-1 coefficients
-    double *dcof;     // dk coefficients
+    double rcof[2 * n] = {};     // z^-2 coefficients
+    double tcof[2 * n] = {};     // z^-1 coefficients
     double parg;      // pole angle
     double sparg;     // sine of pole angle
     double cparg;     // cosine of pole angle
@@ -304,9 +279,6 @@ double *dcof_bwbs( int n, double f1f, double f2f )
     ct = cos(theta);
     s2t = 2.0*st*ct;        // sine of 2*theta
     c2t = 2.0*ct*ct - 1.0;  // cosine 0f 2*theta
-
-    rcof = (double *)calloc( 2 * n, sizeof(double) );
-    tcof = (double *)calloc( 2 * n, sizeof(double) );  
 
     for( k = 0; k < n; ++k )
     {
@@ -320,31 +292,24 @@ double *dcof_bwbs( int n, double f1f, double f2f )
 	tcof[2*k+1] = 2.0*cp*st*cparg/a;
     }
 
-    dcof = trinomial_mult( n, tcof, rcof );
-    free( tcof );
-    free( rcof );
+    trinomial_mult( n, tcof, rcof, dcof );
 
     dcof[1] = dcof[0];
     dcof[0] = 1.0;
     for( k = 3; k <= 2*n; ++k )
         dcof[k] = dcof[2*k-2];
-    return( dcof );
 }
 
 /**********************************************************************
   ccof_bwlp - calculates the c coefficients for a butterworth lowpass 
-  filter. The coefficients are returned as an array of integers.
-
+  filter.
+  ccof  - Pointer to the coefficients array of integers. The overall size is n+1.
 */
 
-int *ccof_bwlp( int n )
+static void ccof_bwlp( int n, int *ccof )
 {
-    int *ccof;
     int m;
     int i;
-
-    ccof = (int *)calloc( n+1, sizeof(int) );
-    if( ccof == NULL ) return( NULL );
 
     ccof[0] = 1;
     ccof[1] = n;
@@ -356,47 +321,36 @@ int *ccof_bwlp( int n )
     }
     ccof[n-1] = n;
     ccof[n] = 1;
-
-    return( ccof );
 }
 
 /**********************************************************************
   ccof_bwhp - calculates the c coefficients for a butterworth highpass 
-  filter. The coefficients are returned as an array of integers.
-
+  filter.
+  ccof  - Pointer to the coefficients array of integers. The overall size is n+1.
 */
 
-int *ccof_bwhp( int n )
+static void ccof_bwhp( int n, int *ccof )
 {
-    int *ccof;
     int i;
 
-    ccof = ccof_bwlp( n );
-    if( ccof == NULL ) return( NULL );
+    ccof_bwlp( n, ccof );
 
     for( i = 0; i <= n; ++i)
         if( i % 2 ) ccof[i] = -ccof[i];
-
-    return( ccof );
 }
 
 /**********************************************************************
   ccof_bwbp - calculates the c coefficients for a butterworth bandpass 
   filter. The coefficients are returned as an array of integers.
-
+  ccof  - Pointer to the coefficients array of integers. The overall size is 2*n+1.
 */
 
-int *ccof_bwbp( int n )
+static void ccof_bwbp( int n , int *ccof)
 {
-    int *tcof;
-    int *ccof;
+    int tcof[2 * n] = {};
     int i;
 
-    ccof = (int *)calloc( 2*n+1, sizeof(int) );
-    if( ccof == NULL ) return( NULL );
-
-    tcof = ccof_bwhp(n);
-    if( tcof == NULL ) return( NULL );
+    ccof_bwhp(n, tcof);
 
     for( i = 0; i < n; ++i)
     {
@@ -404,26 +358,20 @@ int *ccof_bwbp( int n )
         ccof[2*i+1] = 0.0;
     }
     ccof[2*n] = tcof[n];
-
-    free( tcof );
-    return( ccof );
 }
 
 /**********************************************************************
   ccof_bwbs - calculates the c coefficients for a butterworth bandstop 
-  filter. The coefficients are returned as an array of integers.
-
+  filter.
+  ccof  - Pointer to the coefficients array of doubles. The overall size is 2*n+1.
 */
 
-double *ccof_bwbs( int n, double f1f, double f2f )
+static void ccof_bwbs( int n, double f1f, double f2f, double *ccof )
 {
     double alpha;
-    double *ccof;
     int i, j;
 
     alpha = -2.0 * cos(M_PI * (f2f + f1f) / 2.0) / cos(M_PI * (f2f - f1f) / 2.0);
-
-    ccof = (double *)calloc( 2*n+1, sizeof(double) );
 
     ccof[0] = 1.0;
 
@@ -439,8 +387,6 @@ double *ccof_bwbs( int n, double f1f, double f2f )
 	ccof[2] += alpha * ccof[1] + 1.0;
 	ccof[1] += alpha;
     }
-
-    return( ccof );
 }
 
 /**********************************************************************
@@ -450,9 +396,9 @@ double *ccof_bwbs( int n, double f1f, double f2f )
 
 */
 
-double sf_bwlp( int n, double fcf )
+static double sf_bwlp( int n, double fcf )
 {
-    int m, k;         // loop variables
+    int k;         // loop variables
     double omega;     // M_PI * fcf
     double fomega;    // function of omega
     double parg0;     // zeroth pole angle
@@ -462,7 +408,6 @@ double sf_bwlp( int n, double fcf )
     fomega = sin(omega);
     parg0 = M_PI / (double)(2*n);
 
-    m = n / 2;
     sf = 1.0;
     for( k = 0; k < n/2; ++k )
         sf *= 1.0 + fomega * sin((double)(2*k+1)*parg0);
@@ -482,9 +427,9 @@ double sf_bwlp( int n, double fcf )
 
 */
 
-double sf_bwhp( int n, double fcf )
+static double sf_bwhp( int n, double fcf )
 {
-    int m, k;         // loop variables
+    int k;         // loop variables
     double omega;     // M_PI * fcf
     double fomega;    // function of omega
     double parg0;     // zeroth pole angle
@@ -494,7 +439,6 @@ double sf_bwhp( int n, double fcf )
     fomega = sin(omega);
     parg0 = M_PI / (double)(2*n);
 
-    m = n / 2;
     sf = 1.0;
     for( k = 0; k < n/2; ++k )
         sf *= 1.0 + fomega * sin((double)(2*k+1)*parg0);
@@ -514,7 +458,7 @@ double sf_bwhp( int n, double fcf )
 
 */
 
-double sf_bwbp( int n, double f1f, double f2f )
+static double sf_bwbp( int n, double f1f, double f2f )
 {
     int k;            // loop variables
     double ctt;       // cotangent of theta
@@ -550,7 +494,7 @@ double sf_bwbp( int n, double f1f, double f2f )
 
 */
 
-double sf_bwbs( int n, double f1f, double f2f )
+static double sf_bwbs( int n, double f1f, double f2f )
 {
     int k;            // loop variables
     double tt;        // tangent of theta
@@ -577,4 +521,93 @@ double sf_bwbs( int n, double f1f, double f2f )
     }
 
     return( 1.0 / sfr );
+}
+
+static void coeffs_copy( int n, int *ccof, int ccof_n, double *dcof, int dcof_n, double sff, float *coeffs )
+{
+    int i = 0;
+    for (i = 0; i < ccof_n; ++i)
+	coeffs[i] = ccof[i] * sff;
+
+    for (int j = 1; j <= dcof_n; ++i,++j)
+	coeffs[i] = dcof[j];
+}
+
+static void coeffs_copy_d( int n, double *ccof, int ccof_n, double *dcof, int dcof_n, double sff, float *coeffs )
+{
+    int i = 0;
+    for (i = 0; i < ccof_n; ++i)
+	coeffs[i] = ccof[i] * sff;
+
+    for (int j = 1; j <= dcof_n; ++i,++j)
+	coeffs[i] = dcof[j];
+}
+
+// coeffs - Pointer to the coefficients array. The overall size is 2n+1.
+void butter_lp( int n, float fcf, float *coeffs )
+{
+    double dcof[n * 2] = {};
+    int ccof[n + 1] = {};
+
+    dcof_bwlp( n, fcf, dcof );
+    ccof_bwlp( n, ccof);
+    double sff = sf_bwlp( n, fcf );
+
+    coeffs_copy( n, ccof, n + 1, dcof, n, sff, coeffs );
+}
+
+// coeffs - Pointer to the coefficients array. The overall size is 2n+1.
+void butter_hp( int n, float fcf, float *coeffs )
+{
+    double dcof[n * 2] = {};
+    int ccof[n + 1] = {};
+
+    dcof_bwhp( n, fcf, dcof );
+    ccof_bwhp( n, ccof);
+    double sff = sf_bwhp( n, fcf );
+
+    coeffs_copy( n, ccof, n + 1, dcof, n, sff, coeffs );
+}
+
+// coeffs - Pointer to the coefficients array. The overall size is 4n+1.
+void butter_bp( int n, float f1f, float f2f, float *coeffs )
+{
+    double dcof[n * 4] = {};
+    int ccof[2 * n + 1] = {};
+
+    dcof_bwbp( n, f1f, f2f, dcof );
+    ccof_bwbp( n, ccof );
+    double sff = sf_bwbp( n, f1f, f2f );
+
+    coeffs_copy( n, ccof, 2 * n + 1, dcof, 2 * n, sff, coeffs );
+}
+
+// coeffs - Pointer to the coefficients array. The overall size is 4n+1.
+void butter_bs( int n, float f1f, float f2f, float *coeffs )
+{
+    double dcof[n * 4] = {};
+    double ccof[2 * n + 1] = {};
+
+    dcof_bwbs( n, f1f, f2f, dcof );
+    ccof_bwbs( n, f1f, f2f, ccof );
+    double sff = sf_bwbs( n, f1f, f2f );
+
+    coeffs_copy_d(n, ccof, 2 * n + 1, dcof, 2 * n, sff, coeffs);
+}
+
+void butter_stage_arrange_arm( int n, int stage, float *coeffs )
+{
+	assert(stage == 1);
+	assert(n <= 2 * stage);
+	if (n < 2 * stage)
+	{
+		coeffs[3] = -coeffs[2];
+		coeffs[2] = 0.0f;
+		coeffs[4] = 0.0f;
+	}
+	else
+	{
+		coeffs[3] = -coeffs[3];
+		coeffs[4] = -coeffs[4];
+	}
 }
--- src/butter.h
+++ src/butter.h
@@ -1,17 +1,7 @@
-double *binomial_mult( int n, double *p );
-double *trinomial_mult( int n, double *b, double *c );
+#pragma once
 
-double *dcof_bwlp( int n, double fcf );
-double *dcof_bwhp( int n, double fcf );
-double *dcof_bwbp( int n, double f1f, double f2f );
-double *dcof_bwbs( int n, double f1f, double f2f );
-
-int *ccof_bwlp( int n );
-int *ccof_bwhp( int n );
-int *ccof_bwbp( int n );
-double *ccof_bwbs( int n, double f1f, double f2f );
-
-double sf_bwlp( int n, double fcf );
-double sf_bwhp( int n, double fcf );
-double sf_bwbp( int n, double f1f, double f2f );
-double sf_bwbs( int n, double f1f, double f2f );
+void butter_lp( int n, float fcf, float *coeffs );
+void butter_hp( int n, float fcf, float *coeffs );
+void butter_bp( int n, float f1f, float f2f, float *coeffs );
+void butter_bs( int n, float f1f, float f2f, float *coeffs );
+void butter_stage_arrange_arm( int n, int stage, float *coeffs );
