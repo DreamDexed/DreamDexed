55216b33ea95e2656ed673f2d739eb16ba67fe89
Gergo Koteles <soyer@irl.hu>
2026-01-21 15:49:17 +0100

    zyn: Sympathetic: convert to tab indentation

--- src/zyn/Sympathetic.h
+++ src/zyn/Sympathetic.h
@@ -34,52 +34,51 @@ const float guitar_freqs[6] = {82.4f, 110.0f, 146.8f, 196.0f, 246.9f, 329.6f};
 
 class Sympathetic final:public Effect
 {
+public:
+	Sympathetic(EffectParams pars);
+	~Sympathetic();
+	void out(const Stereo<float *> &smp);
+	unsigned char getpresetpar(unsigned char npreset, unsigned int npar);
+	void setpreset(unsigned char npreset);
+	void changepar(int npar, unsigned char value);
+	unsigned char getpar(int npar) const;
+	void cleanup(void);
+	void applyfilters(float *efxoutl, float *efxoutr);
 
-    public:
-    Sympathetic(EffectParams pars);
-    ~Sympathetic();
-        void out(const Stereo<float *> &smp);
-        unsigned char getpresetpar(unsigned char npreset, unsigned int npar);
-        void setpreset(unsigned char npreset);
-        void changepar(int npar, unsigned char value);
-        unsigned char getpar(int npar) const;
-        void cleanup(void);
-        void applyfilters(float *efxoutl, float *efxoutr);
+	static rtosc::Ports ports;
+private:
+	//Parameters
+	unsigned char Pvolume; //Volume or E/R
+	unsigned char Pdrive; //the input amplification
+	unsigned char Plevel; //the output amplification
+	unsigned char Ptype; //Distorsion type
+	unsigned char Pnegate; //if the input is negated
+	unsigned char Plpf; //lowpass filter
+	unsigned char Phpf; //highpass filter
+	unsigned char Pstereo; //0=mono, 1=stereo
+	unsigned char Pq; //0=0.95 ... 127=1.05
+	unsigned char Punison_size; //number of unison strings
+	unsigned char Punison_frequency_spread;
+	unsigned char Pstrings; //number of strings
+	unsigned char Pbasenote; //midi note of lowest string
 
-        static rtosc::Ports ports;
-    private:
-        //Parameters
-        unsigned char Pvolume;       //Volume or E/R
-        unsigned char Pdrive;        //the input amplification
-        unsigned char Plevel;        //the output amplification
-        unsigned char Ptype;         //Distorsion type
-        unsigned char Pnegate;       //if the input is negated
-        unsigned char Plpf;          //lowpass filter
-        unsigned char Phpf;          //highpass filter
-        unsigned char Pstereo;       //0=mono, 1=stereo
-        unsigned char Pq;            //0=0.95 ... 127=1.05
-        unsigned char Punison_size;  //number of unison strings
-        unsigned char Punison_frequency_spread;
-        unsigned char Pstrings;      //number of strings
-        unsigned char Pbasenote;     //midi note of lowest string
+	unsigned char spread, spread_old;
 
-        unsigned char spread, spread_old;
+	float freqs[NUM_SYMPATHETIC_STRINGS];
+	float baseFreq;
 
-        float freqs[NUM_SYMPATHETIC_STRINGS];
-        float baseFreq;
+	void setvolume(unsigned char _Pvolume);
+	void setlpf(unsigned char _Plpf);
+	void sethpf(unsigned char _Phpf);
+	void calcFreqs();
+	void calcFreqsGeneric();
+	void calcFreqsPiano();
+	void calcFreqsGuitar();
 
-        void setvolume(unsigned char _Pvolume);
-        void setlpf(unsigned char _Plpf);
-        void sethpf(unsigned char _Phpf);
-        void calcFreqs();
-        void calcFreqsGeneric();
-        void calcFreqsPiano();
-        void calcFreqsGuitar();
+	//Real Parameters
+	class AnalogFilter *lpfl, *lpfr, *hpfl, *hpfr;
 
-        //Real Parameters
-        class AnalogFilter * lpfl, *lpfr, *hpfl, *hpfr;
-
-        class CombFilterBank * filterBank;
+	class CombFilterBank *filterBank;
 };
 
 }
