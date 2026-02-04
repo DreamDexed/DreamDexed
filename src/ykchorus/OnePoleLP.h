282fd014bd3b37545f852ad8898ccac892b5d4a2
Gergo Koteles <soyer@irl.hu>
2026-02-04 03:44:09 +0100

    YKChorus: modernize

--- src/ykchorus/OnePoleLP.h
+++ src/ykchorus/OnePoleLP.h
@@ -23,19 +23,22 @@
 
 #pragma once
 
-class OnePoleLP {
+class OnePoleLP
+{
 public:
-    float inputs, outputs, lastOutput;
+	float inputs, outputs, lastOutput;
 
-    OnePoleLP() {
-        lastOutput = inputs = outputs = 0.0f;
-    }
+	OnePoleLP()
+	{
+		lastOutput = inputs = outputs = 0.0f;
+	}
 
-    ~OnePoleLP() {}
+	~OnePoleLP() {}
 
-    void tick(float *sample, float cutoff) {
-        float p = (cutoff * 0.98f) * (cutoff * 0.98f) * (cutoff * 0.98f) * (cutoff * 0.98f);
-        outputs = (1.0f - p) * (*sample) + p * outputs;
-        *sample = outputs;
-    }
+	void tick(float *sample, float cutoff)
+	{
+		float p = (cutoff * 0.98f) * (cutoff * 0.98f) * (cutoff * 0.98f) * (cutoff * 0.98f);
+		outputs = (1.0f - p) * (*sample) + p * outputs;
+		*sample = outputs;
+	}
 };
