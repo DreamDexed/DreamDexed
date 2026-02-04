caefe9065bbe47e5f4a04a2182bab17e75a9bc8d
Gergo Koteles <soyer@irl.hu>
2026-02-04 04:36:57 +0100

    arm functions: modernize

--- src/arm_zip_f32.h
+++ src/arm_zip_f32.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "arm_math_types.h"
+#include <stdint.h>
 
 #ifdef __cplusplus
 extern "C"
@@ -8,13 +8,13 @@ extern "C"
 #endif
 
 /**
-* @brief zip two floating-point vector.
-* @param[in]  pSrc1      points to the input vector 1
-* @param[in]  pSrc2      points to the input vector 2
-* @param[out] pDst       points to the output vector
-* @param[in]  blockSize  number of samples in the vector
-*/
-void arm_zip_f32(const float32_t * pSrc1, const float32_t * pSrc2, float32_t * pDst, uint32_t blockSize);
+ * @brief zip two floating-point vector.
+ * @param[in]  pSrc1      points to the input vector 1
+ * @param[in]  pSrc2      points to the input vector 2
+ * @param[out] pDst       points to the output vector
+ * @param[in]  blockSize  number of samples in the vector
+ */
+void arm_zip_f32(const float *pSrc1, const float *pSrc2, float *pDst, uint32_t blockSize);
 
 #ifdef __cplusplus
 }
