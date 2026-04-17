DreamDexed/DreamDexed

commit 8b57157381915d3ce51afe8d6a8e72b117f33a49

CUIBUttons: add dec and inc trigger modes

src/uibuttons.cpp
@@ -37,6 +37,8 @@ CUIButton::CUIButton (void)
 	m_clickEvent(BtnEventNone),
 	m_doubleClickEvent(BtnEventNone),
 	m_longPressEvent(BtnEventNone),
+	m_decEvent(BtnEventNone),
+	m_incEvent(BtnEventNone),
 	m_doubleClickTimeout(0),
 	m_longPressTimeout(0)
 {
@@ -102,6 +104,16 @@ void CUIButton::setLongPressEvent(BtnEvent longPressEvent)
 	m_longPressEvent = longPressEvent;
 }
 
+void CUIButton::setDecEvent(BtnEvent decEvent)
+{
+	m_decEvent = decEvent;
+}
+
+void CUIButton::setIncEvent(BtnEvent incEvent)
+{
+	m_incEvent = incEvent;
+}
+
 unsigned CUIButton::getPinNumber(void)
 {
 	return m_pinNumber;
@@ -110,6 +122,7 @@ unsigned CUIButton::getPinNumber(void)
 CUIButton::BtnTrigger CUIButton::ReadTrigger (void)
 {
 	unsigned value;
+
 	if (isMidiPin(m_pinNumber))
 	{
 		if (!m_midipin)
@@ -117,8 +130,23 @@ CUIButton::BtnTrigger CUIButton::ReadTrigger (void)
 			// Always return "not pressed" if not configured
 			return BtnTriggerNone;
 		}
-		// Simulates a PULLUP IO pin, so "true" is LOW (0)
-		value = m_midipin->Read() < MIDIPIN_CENTER ? HIGH : LOW;
+
+		if (m_decEvent || m_incEvent)
+		{
+			value = m_midipin->Read();
+
+			if (value == MIDIPIN_CENTER)
+				return BtnTriggerNone;
+
+			// reset value to trigger only once
+			m_midipin->Write(MIDIPIN_CENTER);
+			return value < MIDIPIN_CENTER ? BtnTriggerDec : BtnTriggerInc;
+		}
+		else
+		{
+			// Simulates a PULLUP IO pin, so "true" is LOW (0)
+			value = m_midipin->Read() < MIDIPIN_CENTER ? HIGH : LOW;
+		}
 	}
 	else
 	{
@@ -232,6 +260,12 @@ CUIButton::BtnEvent CUIButton::Read (void) {
 	else if (trigger == BtnTriggerLongPress) {
 		return m_longPressEvent;
 	}
+	else if (trigger == BtnTriggerDec) {
+		return m_decEvent;
+	}
+	else if (trigger == BtnTriggerInc) {
+		return m_incEvent;
+	}
 
 	assert (trigger == BtnTriggerNone);
 
@@ -251,6 +285,12 @@ CUIButton::BtnTrigger CUIButton::triggerTypeFromString(const char* triggerString
 	}	
 	else if (strcmp(triggerString, "longpress") == 0) {
 		return BtnTriggerLongPress;
+	}
+	else if (strcmp(triggerString, "dec") == 0) {
+		return BtnTriggerDec;
+	}
+	else if (strcmp(triggerString, "inc") == 0) {
+		return BtnTriggerInc;
 	}
 
 	LOGERR("Invalid action: %s", triggerString);
@@ -341,7 +381,7 @@ boolean CUIButtons::Initialize (void)
 
 	// Each normal button can be assigned up to 3 actions: click, doubleclick and
 	// longpress. We may not initialise all of the buttons.
-	// MIDI Buttons can be assigned to click, doubleclick, longpress
+	// MIDI buttons can be assigned to click, doubleclick, longpress, dec, inc
 	unsigned pins[MAX_BUTTONS] = {
 		m_prevPin, m_nextPin, m_backPin, m_selectPin, m_homePin, m_pgmUpPin,  m_pgmDownPin,  m_BankUpPin,  m_BankDownPin, m_TGUpPin,  m_TGDownPin, 
 		m_prevMidi, m_nextMidi, m_backMidi, m_selectMidi, m_homeMidi, m_pgmUpMidi, m_pgmDownMidi, m_BankUpMidi, m_BankDownMidi, m_TGUpMidi, m_TGDownMidi
@@ -451,6 +491,12 @@ void CUIButtons::bindButton(unsigned pinNumber, CUIButton::BtnTrigger trigger, C
 			}
 			else if (trigger == CUIButton::BtnTriggerLongPress) {
 				m_buttons[i].setLongPressEvent(event);
+			}
+			else if (trigger == CUIButton::BtnTriggerDec) {
+				m_buttons[i].setDecEvent(event);
+			}
+			else if (trigger == CUIButton::BtnTriggerInc) {
+				m_buttons[i].setIncEvent(event);
 			}
 			else {
 				assert (trigger == CUIButton::BtnTriggerNone);

src/config.h
@@ -231,7 +231,7 @@ public:
 	unsigned GetMIDIButtonSelect (void) const;
 	unsigned GetMIDIButtonHome (void) const;
 
-	// Action type for Midi buttons: "click", "doubleclick", "longpress", ""
+	// Action type for Midi buttons: "click", "doubleclick", "longpress", "dec", "inc", ""
 	const char *GetMIDIButtonActionPrev (void) const;
 	const char *GetMIDIButtonActionNext (void) const;
 	const char *GetMIDIButtonActionBack (void) const;
@@ -246,7 +246,7 @@ public:
 	unsigned GetMIDIButtonTGUp (void) const;
 	unsigned GetMIDIButtonTGDown (void) const;
 	
-	// Action type for buttons: "click", "doubleclick", "longpress", ""
+	// Action type for buttons: "click", "doubleclick", "longpress", "dec", "inc", ""
 	const char *GetMIDIButtonActionPgmUp (void) const;
 	const char *GetMIDIButtonActionPgmDown (void) const;
 	const char *GetMIDIButtonActionBankUp (void) const;

src/minidexed.ini
@@ -128,6 +128,7 @@ LongPressTimeout=400
 # MIDI Button Navigation
 #  Specify MIDI CC to act as a button (0 = ununsed, so don't use CC 0)
 #  NB: Off < 64 < ON for click / doubleclick / longpress actions
+#      DEC < 64 < INC for dec / inc actions
 # CC channel: 0=OFF; 1-16 MIDI Ch; >16 Omni
 # If MIDIButtonNotes>0 then treat MIDIButton numbers as MIDI
 # Note numbers, triggered with NoteOn/NoteOff, not CC numbers.

src/uibuttons.h
@@ -41,7 +41,9 @@ public:
 		BtnTriggerNone = 0,
 		BtnTriggerClick = 1,
 		BtnTriggerDoubleClick = 2,
-		BtnTriggerLongPress = 3
+		BtnTriggerLongPress = 3,
+		BtnTriggerDec = 4,
+		BtnTriggerInc = 5,
 	};
 
 	enum BtnEvent
@@ -70,6 +72,8 @@ public:
 	void setClickEvent(BtnEvent clickEvent);
 	void setDoubleClickEvent(BtnEvent doubleClickEvent);
 	void setLongPressEvent(BtnEvent longPressEvent);
+	void setDecEvent(BtnEvent decEvent);
+	void setIncEvent(BtnEvent incEvent);
 
 	unsigned getPinNumber(void);
 	
@@ -100,7 +104,11 @@ private:
 	BtnEvent m_doubleClickEvent;
 	// Event to fire on long press
 	BtnEvent m_longPressEvent;
-	
+	// Event to fire on dec
+	BtnEvent m_decEvent;
+	// Event to fire on inc
+	BtnEvent m_incEvent;
+
 	// Timeout for double click in tenths of a millisecond
 	unsigned m_doubleClickTimeout;
 	// Timeout for long press in tenths of a millisecond

commit 6c5f97c68708da06993f528927f551a131b8560e

CUIButton: initialize midipin to center in dec/inc mode
Fixes: 8b57157
("CUIBUttons: add dec and inc trigger modes")

src/uibuttons.cpp
@@ -118,11 +118,13 @@ void CUIButton::setLongPressEvent(BtnEvent longPressEvent)
 void CUIButton::setDecEvent(BtnEvent decEvent)
 {
 	m_decEvent = decEvent;
+	if (m_midipin) m_midipin->Write(MIDIPIN_CENTER);
 }
 
 void CUIButton::setIncEvent(BtnEvent incEvent)
 {
 	m_incEvent = incEvent;
+	if (m_midipin) m_midipin->Write(MIDIPIN_CENTER);
 }
 
 unsigned CUIButton::getPinNumber()